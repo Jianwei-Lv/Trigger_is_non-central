eb7206e8852b5cd150179633e5be9c7e35bd7631
Yueguang Bai <76138339+Jianwei-Lv@users.noreply.github.com>
2023-04-12 10:29:12 +0800

    Add files via upload

---
+++ transformer/sublayers.py
@@ -0,0 +1,170 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+from transformer.modules import Linear
+from transformer.modules import ScaledDotProductAttention
+from transformer.modules import LayerNormalization
+
+# from .modules import Linear
+# from .modules import ScaledDotProductAttention
+# from .modules import LayerNormalization
+
+
+class _MultiHeadAttention(nn.Module):
+    def __init__(self, d_k, d_v, d_model, n_heads, dropout):
+        super(_MultiHeadAttention, self).__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+        self.d_model = d_model
+        self.n_heads = n_heads
+
+        self.w_q = Linear(d_model, d_k * n_heads)
+        self.w_k = Linear(d_model, d_k * n_heads)
+        self.w_v = Linear(d_model, d_v * n_heads)
+
+        self.attention = ScaledDotProductAttention(d_k, dropout)
+
+        self.attn_line = nn.Linear(70, 35)
+        self.dropout=nn.Dropout(0.4)
+
+        # self.attn_lines=nn.ModuleList()
+        #
+        # for _ in range(5):
+        #     self.attn_lines.append(nn.Linear(in_features=70,
+        #                                 out_features=35,
+        #                                 bias=True))
+
+        # self.cnn = nn.Conv1d(70, 35, kernel_size=5, padding=2)
+        # self.cnn1 = nn.Conv1d(50, 35, kernel_size=7, padding=3)
+    def forward(self, q, k, v, attn_mask,scale_factor=True,attn_temp=None):
+        # q: [b_size x len_q x d_model]
+        # k: [b_size x len_k x d_model]
+        # v: [b_size x len_k x d_model]
+        b_size = q.size(0)
+
+        # q_s: [b_size x n_heads x len_q x d_k]
+        # k_s: [b_size x n_heads x len_k x d_k]
+        # v_s: [b_size x n_heads x len_k x d_v]
+        q_s = self.w_q(q).view(b_size, -1, self.n_heads, self.d_k).transpose(1, 2)
+        k_s = self.w_k(k).view(b_size, -1, self.n_heads, self.d_k).transpose(1, 2)
+        v_s = self.w_v(v).view(b_size, -1, self.n_heads, self.d_v).transpose(1, 2)
+
+        if True:  # attn_mask: [b_size x len_q x len_k]
+            attn_mask = attn_mask.unsqueeze(1).repeat(1, self.n_heads, 1, 1)
+        # context: [b_size x n_heads x len_q x d_v], attn: [b_size x n_heads x len_q x len_k]
+        context, attn = self.attention(q_s, k_s, v_s, attn_mask=attn_mask,scale_factor=scale_factor,attn_=attn_temp)
+        # context: [b_size x len_q x n_heads * d_v]
+        context = context.transpose(1, 2).contiguous().view(b_size, -1, self.n_heads * self.d_v)
+        attn_=torch.mean(attn,1).squeeze(1)
+
+        if not scale_factor and attn_temp is not None:
+            # for i in range(5):
+            attn_ = attn_temp+attn_
+            # attn_final = torch.cat([attn_temp, attn_], -1)
+            # attn_=self.attn_line(attn_final)
+            # sigmoid =torch.sigmoid(attn_final)
+            # attn_ = sigmoid * attn_ + (1 - sigmoid) * attn_temp
+            # attn_=self.dropout(attn_)
+
+            # attn_final=attn_final.unsqueeze(0)
+
+            # attn_final = attn_final.transpose(1, 2).contiguous()
+            # attn_final = self.cnn(attn_final)
+            # attn_final = self.cnn1(attn_final)
+            # attn_final = attn_final.transpose(1, 2).contiguous()
+            # attn_final = attn_final.squeeze()
+            # attn_ = self.dropout(attn_final)
+        # return the context and attention weights
+        return context, attn_
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_k, d_v, d_model, n_heads, dropout):
+        super(MultiHeadAttention, self).__init__()
+        self.n_heads = n_heads
+        self.multihead_attn = _MultiHeadAttention(d_k, d_v, d_model, n_heads, dropout)
+        self.proj = Linear(n_heads * d_v, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = LayerNormalization(d_model)
+
+    def forward(self, q, k, v, attn_mask,scale_factor=True,attn_=None):
+        # q: [b_size x len_q x d_model]
+        # k: [b_size x len_k x d_model]
+        # v: [b_size x len_v x d_model] note (len_k == len_v)
+        residual = q
+        # context: a tensor of shape [b_size x len_q x n_heads * d_v]
+        context, attn = self.multihead_attn(q, k, v, attn_mask=attn_mask,scale_factor=scale_factor,attn_temp=attn_)
+
+        # project back to the residual size, outputs: [b_size x len_q x d_model]
+        output = self.dropout(self.proj(context))
+        return self.layer_norm(residual + output), attn
+
+
+class MultiBranchAttention(nn.Module):
+    def __init__(self, d_k, d_v, d_model, d_ff, n_branches, dropout):
+        super(MultiBranchAttention, self).__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.n_branches = n_branches
+
+        self.multihead_attn = _MultiHeadAttention(d_k, d_v, d_model, n_branches, dropout)
+        # additional parameters for BranchedAttention
+        self.w_o = nn.ModuleList([Linear(d_v, d_model) for _ in range(n_branches)])
+        self.w_kp = torch.rand(n_branches)
+        self.w_kp = nn.Parameter(self.w_kp/self.w_kp.sum())
+        self.w_a = torch.rand(n_branches)
+        self.w_a = nn.Parameter(self.w_a/self.w_a.sum())
+
+        self.pos_ffn = nn.ModuleList([
+            PoswiseFeedForwardNet(d_model, d_ff//n_branches, dropout) for _ in range(n_branches)])
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = LayerNormalization(d_model)
+
+        init.xavier_normal(self.w_o)
+
+    def forward(self, q, k, v, attn_mask):
+        # q: [b_size x len_q x d_model]
+        # k: [b_size x len_k x d_model]
+        # v: [b_size x len_v x d_model] note (len_k == len_v)
+        residual = q
+
+        # context: a tensor of shape [b_size x len_q x n_branches * d_v]
+        context, attn = self.multih_attn(q, k, v, attn_mask=attn_mask)
+
+        # context: a list of tensors of shape [b_size x len_q x d_v] len: n_branches
+        context = context.split(self.d_v, dim=-1)
+
+        # outputs: a list of tensors of shape [b_size x len_q x d_model] len: n_branches
+        outputs = [self.w_o[i](context[i]) for i in range(self.n_branches)]
+        outputs = [kappa * output for kappa, output in zip(self.w_kp, outputs)]
+        outputs = [pos_ffn(output) for pos_ffn, output in zip(self.pos_ffn, outputs)]
+        outputs = [alpha * output for alpha, output in zip(self.w_a, outputs)]
+
+        # output: [b_size x len_q x d_model]
+        output = self.dropout(torch.stack(outputs).sum(dim=0))
+        return self.layer_norm(residual + output), attn
+
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = LayerNormalization(d_model)
+
+    def forward(self, inputs):
+        # inputs: [b_size x len_q x d_model]
+        residual = inputs
+        output = self.relu(self.conv1(inputs.transpose(1, 2)))
+
+        # outputs: [b_size x len_q x d_model]
+        output = self.conv2(output).transpose(1, 2)
+        output = self.dropout(output)
+
+        return self.layer_norm(residual + output)
